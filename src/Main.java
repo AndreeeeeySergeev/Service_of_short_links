98e42f3f071204be3c4f616053cd5522bee37df5
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2025-01-17 16:01:14 +0300

    Creating UUID of user and checking UUID

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
-import hashSet
+import java.util.Objects;
+import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Set<String> set = new HashSet<>();
+
+        System.out.print("Здравствуйте! Введите ваш UUID или напишите 'нет': ");
+        String choose = input.nextLine();
+        if (choose.contains("нет")) {
+            UUID uuid = UUID.randomUUID();
+            set.add(String.valueOf(uuid));
+            System.out.println(uuid);
+        } else {
+            System.out.println(set.contains(choose));
         }
+
     }
 }
 
-public UUID () {
-    UUID uuid = UUID.randomUUID();
-}
+
+//public UUID () {
+//    Set<String> set = new HashSet<>();
+//    UUID uuid = UUID.randomUUID();
+//    uuid.toString();
+//    set.add(uuid);
+
+
+//}
