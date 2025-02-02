5113d7aff74d7fdcdf3147f0d4b54c9cbd7b0fbb
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2025-02-02 23:37:49 +0300

    Created some methods

--- src/Main.java
+++ src/Main.java
@@ -3,137 +3,152 @@ import java.util.*;
 
 
 public class Main {
+    static HashMap<String, String> urls = new HashMap<>();
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         menu();
     }
-        public static void menu() throws IOException, ClassNotFoundException {
-            Scanner input = new Scanner(System.in);
-            Set<String> set = new HashSet<>();
 
-            System.out.print("Здравствуйте! Введите ваш UUID или напишите 'нет' или 'выход', для выхода: ");
-            String choose = input.nextLine();
-            // проверяем существование UUID
-            if (choose.contains("нет")) {
-                UUID uuid = UUID.randomUUID();
-                set.add(String.valueOf(uuid));
-                File your_uuid = new File("your_uuid.txt");
-                ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(your_uuid));
-                output.writeObject(set); // to do перекодировать файл в utf-8
-                output.close();
-                System.out.println(uuid);
-                System.out.println(set.contains(String.valueOf(uuid))); // проверка для себя
-                menu();
-            } else if (choose.contains("выход")) {
-                System.out.println("До свидания!");
-                System.exit(1);
+    public static void menu() throws IOException, ClassNotFoundException {
+
+        Set<String> set = new HashSet<>();
+
+        System.out.print("Здравствуйте! Введите ваш UUID или напишите 'нет' или 'выход', для выхода: ");
+        String choose = input.nextLine();
+        // проверяем существование UUID
+        if (choose.contains("нет")) {
+            UUID uuid = UUID.randomUUID();
+            set.add(String.valueOf(uuid));
+            File your_uuid = new File("your_uuid.txt");
+            ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(your_uuid));
+            output.writeObject(set); // to do перекодировать файл в utf-8
+            output.close();
+            System.out.println("Ваш uuid: " + uuid);
+            System.out.println(set.contains(String.valueOf(uuid))); // проверка для себя
+            menu();
+        } else if (choose.contains("выход")) {
+            System.out.println("До свидания!");
+            System.exit(1);
+        } else {
+            ObjectInputStream ini = new ObjectInputStream(new FileInputStream("your_uuid.txt"));
+            Set<String> aNewSet = (HashSet<String>) ini.readObject(); // to do обернуть считывание с файла в try-catch
+
+            if (aNewSet.contains(choose)) {
+                System.out.println(aNewSet.contains(choose));
+                menu1();
             } else {
-                ObjectInputStream ini = new ObjectInputStream(new FileInputStream("your_uuid.txt"));
-                Set<String> aNewSet = (HashSet<String>) ini.readObject(); // to do обернуть считывание с файла в try-catch
+                System.out.print("Неверный uuid!\n" +
+                        "1.Ввеcти заново uuid\n" +
+                        "2.Выйти\n");
+                String variant = input.nextLine();
 
-                if (aNewSet.contains(choose)) {
-                    System.out.println(aNewSet.contains(choose));
-                    menu();
-                } else {
-                    System.out.print("Неверный uuid!\n" +
-                            "1.Ввеcти заново uuid\n" +
-                            "2.Выйти\n");
-                    String variant = input.nextLine();
-
-                    switch (variant) {
-                        case "1":
-                            menu();
-                            break;
-                        case "2":
-                            break;
-                    }
+                switch (variant) {
+                    case "1":
+                        menu();
+                        break;
+                    case "2":
+                        System.out.println("До новых встреч!");
+                        System.exit(1);
+                        break;
                 }
             }
         }
     }
 
-//    public static void createShortLink() throws FileNotFoundException, IOException {
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Введите вашу ccылку: ");
-//        String link = input.nextLine();
-//        String shortlink = link + UUID.randomUUID().toString().substring(0, 8);// создаем ссылку
-//        System.out.println("Ваша короткая ссылка: " + shortlink);
-//        //link == shortlink;
-//        File myfile = new File("links.txt"); // записываем в файл,
-//        OutputStreamWriter writered = new OutputStreamWriter(new FileOutputStream(myfile));
-//        writered.write(shortlink);// чтобы дальше взаимодействовать с ней
-//        writered.close();
-//    }
-//
-//    public static void deletelink() throws IOException {
-//        try {
-//            InputStreamReader reading = new InputStreamReader(new FileInputStream("links.txt"));
-//            String a = String.valueOf(reading.read());
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Ошибка:" + e.getMessage());
-//        }
-//        Calendar days = Calendar.getInstance();
-//        if (days.after(3)) { // время существования ссылки
-////            a.resolveConstantDesc(createShortLink());
-//            File file = new File("links.txt");
-//            file.delete();
-//            createShortLink();// Здесь получается, что будем снова перезаписывать ссылку после 3 дней
-//        }
-//    }
 
-//        public static void clicking() throws IOException {
-//            Scanner onesmore = new Scanner(System.in);
-//            System.out.print("Введите количество использования ссылки: ");
-//            int good = Integer.valueOf(onesmore.nextLine());
-//            File file = new File("links.txt");
-//            file.exists(); // проверяем существование файла
-////            BufferedReader read = new BufferedReader(file, good); // BufferedInputSTREAM
-//            InputStreamReader reading = new InputStreamReader(new FileInputStream(file));
-//            reading.read();
-//            Integer c = Integer.valueOf(reading.read());
-//            if ( c > good) {
-//                file.delete(); // если превышает количество обращений к файлу ссылки, то файл удаляется
-//        }
-//    }
-//
+    public static void createShortLink() throws FileNotFoundException, IOException {
+        Calendar calendar = Calendar.getInstance();
+        Scanner input = new Scanner(System.in);
 
-//    public static void menu() throws IOException {
-//            Scanner input = new Scanner(System.in);
-//            boolean work = true;
-//            while (work) {
-//                System.out.print("Выберите нужный вам пункт меню:\n " +
-//                        "1. Войти\n " +
-//                        "2. Посчитать доходы\n " +
-//                        "3. Посчитать расходы\n " +
-//                        "4. Установить бюджет\n " +
-//                        "5. Показать итог\n " +
-//                        "6. Посчитать бюджет\n " +
-//                        "7. Выйти\n");
-//                String choose = input.nextLine();
-//
-//                switch (choose) {
-//                    case "1":
-//                        break;
-//                    case "2":
-//
-//                        break;
-//                    case "3":
-//
-//                    case "4":
-//
-//                    case "5":
-//
-//                    case "6":
-//
-//                    case "7":
-//                        System.out.println("До новых встреч!");
-//                        work = false;
-//                        break;
-//                    default:
-//                        throw new IllegalStateException("Unexpected value: " + choose);
-//                }
-//            }
-//        }
+        System.out.print("Введите вашу ccылку: ");
+        String link = input.nextLine();
+        String shortlink = link + UUID.randomUUID().toString().substring(0, 8);// создаем ссылку
+
+        if (urls.get(shortlink).isEmpty()) { //проверяем наличие ссылки
+            urls.put(shortlink, link);
+            System.out.println("Ваша короткая ссылка: " + shortlink);
+        } else {
+            String shortlink1 = shortlink + UUID.randomUUID().toString().substring(0, 15);
+            urls.put(shortlink1, link);
+            System.out.println("Ваша короткая ссылка: " + shortlink1);
+        }
+        File urls_file = new File("urls_file.txt");
+        ObjectOutputStream urls_output = new ObjectOutputStream(new FileOutputStream(urls_file));
+        urls_output.writeObject(urls); // записываем в файл
+        urls_output.close();
+
+        int day = calendar.get(Calendar.DAY_OF_MONTH); // создаем существование ссылки по дате
+        System.out.print("Введите количество дней существования ссылки: ");
+        int c = input.nextInt();
+        if (c > day) {
+            InputStreamReader reading = new InputStreamReader(new FileInputStream("urls_file.txt"));
+            urls.remove(shortlink);
+            System.out.println("Превышено количество дней существования ссылки");
+            menu1();
+        }
+    }
+    public static void getShortlink() throws IOException {
+        try {
+            FileReader in_urls = new FileReader("urls_file.txt");
+            in_urls.read();
+            System.out.print("Введите вашу короткую ссылку: ");
+            String a = input.nextLine();
+            System.out.println("Ваша полноценная ссылка: " + urls.get(a));
+        } catch (FileNotFoundException e) {
+            System.out.println("Ссылки не существует " + e.getMessage());
+            menu1();
+        }
+    }
+
+        public static void clicking() throws IOException {
+            Scanner onesmore = new Scanner(System.in);
+            System.out.print("Введите количество использования ссылки: ");
+            int good = Integer.valueOf(onesmore.nextLine());
+            File file = new File("links.txt");
+            file.exists(); // проверяем существование файла
+//            BufferedReader read = new BufferedReader(file, good); // BufferedInputSTREAM
+            InputStreamReader reading = new InputStreamReader(new FileInputStream(file));
+            reading.read();
+            Integer c = Integer.valueOf(reading.read());
+            if ( c > good) {
+                file.delete(); // если превышает количество обращений к файлу ссылки, то файл удаляется
+        }
+    }
+
+
+    public static void menu1() throws IOException {
+        Scanner input = new Scanner(System.in);
+        boolean work = true;
+        while (work) {
+            System.out.print("Выберите нужный вам пункт меню:\n " +
+                    "1. Cоздать короткую ссылку\n " +
+                    "2. Получить ссылку для перехода\n " +
+                    "3. Количество раз для перехода\n " +
+                    "4. Выйти\n");
+            String choose = input.nextLine();
+
+            switch (choose) {
+                case "1":
+                    createShortLink();
+                    break;
+                case "2":
+                    getShortlink();
+                    break;
+                case "3":
+                    clicking();
+                    break;
+                case "4":
+                    System.out.println("До новых встреч!");
+                    System.exit(1);
+                    work = false;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + choose);
+            }
+        }
+    }
+}
 
 
 
